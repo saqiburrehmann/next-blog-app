4defd3d928aa19f3c7a98ae2db0f87e83f85298c
Saqib Ur Rehman <saqiburrehman75@gmail.com>
2025-05-10 23:51:42 +0500

    Adding Blog Page with dynamic Id

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import { assets } from "@/Assets/assets";
+import Link from "next/link";
 
-const BlogItem = ({ title, description, category, image }) => {
+const BlogItem = ({ title, description, category, image, id }) => {
   return (
     <div className="max-w-[280px] sm:max-w-[260px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]">
-      <Image
-        src={image}
-        alt={"Blog image"}
-        width={350}
-        height={350}
-        className="border-b border-black"
-      />
+      <Link href={`/blogs/${id}`}>
+        <Image
+          src={image}
+          alt={"Blog image"}
+          width={350}
+          height={350}
+          className="border-b border-black"
+        />
+      </Link>
       <p className="ml-5 mt-5 px-1 inline-block bg-black text-white text-sm">
         {category}
       </p>
@@ -22,10 +25,13 @@ const BlogItem = ({ title, description, category, image }) => {
         <p className="mb-3 text-sm tracking-tight text-gray-700">
           {description}
         </p>
-        <div className="inline-flex items-center py-2 font-semibold text-center">
+        <Link
+          href={`/blogs/${id}`}
+          className="inline-flex items-center py-2 font-semibold text-center"
+        >
           Read more{" "}
           <Image src={assets.arrow} alt="" width={12} className="ml-2" />
-        </div>
+        </Link>
       </div>
     </div>
   );
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import { assets } from "@/Assets/assets";
 const Footer = () => {
   return (
     <div className="flex justify-around flex-col gap-2 sm:gap-8 sm:flex-row bg-black py-5 items-center">
-      <Image src={assets.logo_light} width={120} />
+      <Image src={assets.logo_light} width={120} alt="" />
       <p className="text-sm text-white  ">
         All right reserved. Copyright @blogger
       </p>
